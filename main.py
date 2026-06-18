5c6cf4584049952187b986b42e44a8f2d739e40e
inaTrip <hermesmola@gmail.com>
2026-06-18 20:44:30 +0200

    fear: summmarize text via the API

--- main.py
+++ main.py
@@ -7,23 +7,25 @@ import sys
 with open(sys.argv[1], encoding="utf-8") as f:
     contenido = f.read()
 # aquí fuera del bloque, el archivo ya está cerrado
-print(len(contenido))
+#print(len(contenido))
 
 
 # LLAMADA A API CON HOLA MUNDO:
 
-#load_dotenv()
+load_dotenv()
 
 # key = os.getenv("ANTHROPIC_API_KEY") Esto no es necesario se lee solo
 
-#client = Anthropic()
+client = Anthropic()
 
-#model = "claude-haiku-4-5"
-#max_tokens = 100
-#messages = [{"role": "user", "content": "Di hola en una frase."}]
+model = "claude-haiku-4-5"
+max_tokens = 1000
+prompt = "Resume el siguiente texto en castellano, recogiendo sus ideas principales y puntos clave en un párrafo conciso (4-6 frases). Texto:"
 
-#response = client.messages.create(model=model,max_tokens=max_tokens,messages=messages)
+messages = [{"role": "user", "content": f"{prompt}\n\n{contenido}"}]
 
-#print(response)
+response = client.messages.create(model=model,max_tokens=max_tokens,messages=messages)
 
-#print(response.content[0].text)
+print(response)
+
+print(response.content[0].text)
